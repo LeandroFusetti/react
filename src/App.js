165763575c6c938ea13638ba46e9fd6bab3c932b
Leandro Fusetti <leandrofusetti@gmail.com>
2022-05-16 18:31:42 -0300

    clase 3

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import NavBar from "./components/NavBar/NavBar.js";
+import NavBar from "./components/NavBar/NavBar.js"; 
 import './App.css';
+import ItemListContainer from "./components/ItemListContainer/ItemListContainer.js"; 
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <NavBar/>
+        <ItemListContainer greetings="hola coder"/> 
+        
       </header>
     </div>
   );
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,9 +1,12 @@
+import CartWidget from "../CartWidget/CartWidget"
 import "./NavBar.css"
+
 
 const NavBar = () => {
     return(
             <nav className ="gradiente">
                 <div className="posicionamiento">
+                    <CartWidget/>
                     <h1 className= "logo">ALL <span>GAMER</span></h1>
                     <div className= "navbar__div">
                         <button>JUEGOS</button>
