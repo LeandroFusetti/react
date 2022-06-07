9cb91aecf6f451d7f0479af498c2f8225cd74250
LeandroFusetti <92404558+LeandroFusetti@users.noreply.github.com>
2022-06-07 01:20:37 -0300

    Update ItemDetail.js

--- src/components/ItemDetail.js/ItemDetail.js
+++ src/components/ItemDetail.js/ItemDetail.js
@@ -30,7 +30,7 @@ const ItemDetail = ({nombre, precio, foto,foto2,foto3, detalle, stock, }) => {
                 <h1>{nombre}</h1>
                 <p>{detalle}</p>
                 <h2>${precio}</h2>
-                { count > 0  && <Link to='/cart'>Finalizar</Link>}
+                { count > 0  && <Link to={'/cart'}>Finalizar</Link>}
                     
                 { count <= 0   && <ItemCount inicial={1} stock={stock} SumarAlCarrito={confirmarCompra} />}
             </div>
